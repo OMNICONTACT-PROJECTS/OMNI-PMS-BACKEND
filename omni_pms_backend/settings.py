52ef48e574c603b3d754c76ac2e68d8d31179ec7
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-07-17 10:41:06 +0200

    deleted all migrations in agents migrations

--- omni_pms_backend/settings.py
+++ omni_pms_backend/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ##################### SETTINGS FOR RENDER PRODUCTION DEPLOYMENT ####################
 
-# load_dotenv(BASE_DIR / ".env.render-production")
-load_dotenv(BASE_DIR / ".env.local")
-
+load_dotenv(BASE_DIR / ".env.render-production")
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "0").lower() in ["true", "t", "1"]
 # ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(" ")
@@ -115,7 +113,7 @@ WSGI_APPLICATION = "omni_pms_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
