f9273c319cff976aaefa8817491e5be94396880b
Jeremiah Muchazondida <jjmuchazondida@gmail.com>
2024-09-25 12:01:33 +0200

    Saving changes

--- omni_pms_backend/settings.py
+++ omni_pms_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ##################### SETTINGS FOR RENDER PRODUCTION DEPLOYMENT ####################
 
-load_dotenv(BASE_DIR / ".env.omni-production")
+load_dotenv(BASE_DIR / ".env.local")
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "0").lower() in ["true", "t", "1"]
 # ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(" ")
@@ -41,7 +41,7 @@ DEBUG = os.getenv("DEBUG", "0").lower() in ["true", "t", "1"]
 # DEBUG = True
 
 
-ALLOWED_HOSTS = ["omni-pms-backend.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
